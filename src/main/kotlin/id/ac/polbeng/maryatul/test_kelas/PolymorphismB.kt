fa8f610e9fa061a47cdc297a2936a05ae3662272
JALA MEDIA COMP <maryatulkippiyah01@gmail.com>
2022-04-15 11:18:38 +0700

    percobaan polymorphismB

---
+++ src/main/kotlin/id/ac/polbeng/maryatul/test_kelas/PolymorphismB.kt
@@ -0,0 +1,20 @@
+package id.ac.polbeng.maryatul.test_kelas
+
+open class MyBase {
+    // Harus menggunakan pengubah "terbuka" untuk mengizinkan kelas anak menimpanya
+    open fun think () {
+        println("Hey!! i am thinking ")
+    }
+}
+class MyDerived: MyBase() { // pewarisan terjadi menggunakan konstruktor default
+    // Harus menggunakan pengubah "override" untuk mengganti fungsi kelas dasar
+    override fun think() {
+        println("I Am from Child")
+    }
+}
+fun main() {
+    val myBase = MyBase()
+    myBase.think()
+    var myDerived = MyDerived()
+    myDerived.think()
+}
